3118733018a2d34db4ce43bc98cb04903ac7a2d3
dqnk <64268180+dqnk@users.noreply.github.com>
2021-03-12 18:08:56 +0100

    cleaning up

--- interrupt/interrupt.ino
+++ interrupt/interrupt.ino
@@ -8,14 +8,14 @@ volatile int y = 999;
 
 void setup()
 {
-	cli();				  //	disable interrupts
-	DDRB = 0b00000000;	  // 	all digital pins B are inputs
-	DDRD = 0b00000000;	  //	pin 3 is input
-	PCICR |= 0b00000100;  //	turn on interrupts for PORTD
-	PCMSK2 |= 0b00001000; //	turn on interrupts for specific pin (pin 3)
-	DDRB = 0b00001000;	  //	pin 11 is output
+	cli();			//	disable interrupts
+	DDRB = 0b00000000;	// 	all digital pins B are inputs
+	DDRD = 0b00000000;	//	pin 3 is input
+	PCICR |= 0b00000100;	//	turn on interrupts for PORTD
+	PCMSK2 |= 0b00001000;	//	turn on interrupts for specific pin (pin 3)
+	DDRB = 0b00001000;	//	pin 11 is output
 
-	sei(); //	enable interrupts
+	sei();			//	enable interrupts
 	Serial.begin(9600);
 }
 
@@ -23,10 +23,10 @@ void loop()
 {
 	print();
 	delay(1000);
-	PORTB = 0b00001000; //	pin 11 set to high
+	PORTB = 0b00001000;	//	pin 11 set to high
 	print();
 	delay(1000);
-	PORTB = 0b00000000; //	pin 11 set to low
+	PORTB = 0b00000000;	//	pin 11 set to low
 }
 
 inline void print()
@@ -39,14 +39,14 @@ inline void print()
 	Serial.println(z);
 }
 
-ISR(PCINT2_vect) //	Port D, PCINT16 - PCINT23
+ISR(PCINT2_vect)		//	Port D, PCINT16 - PCINT23
 {
 	x++;
-	if (!(PIND & (1 << 3))) //	only falling edge
+	if (!(PIND & (1 << 3)))	//	only falling edge
 	{
 		z++;
 	}
-	if (PIND & (1 << 3)) //	only rising edge
+	if (PIND & (1 << 3))	//	only rising edge
 	{
 		y--;
 	}
