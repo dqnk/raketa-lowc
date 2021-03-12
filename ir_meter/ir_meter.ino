7c3a0866b18129682c5ffefac7ad8321dd736614
dqnk <64268180+dqnk@users.noreply.github.com>
2021-03-12 21:53:24 +0100

    cleaned up comments

--- ir_meter/ir_meter.ino
+++ ir_meter/ir_meter.ino
@@ -2,11 +2,11 @@ volatile int count = 0; // volatile because accessed during interrupts
 
 void setup()
 {
-	cli();				  //	disable interrupts
-	DDRB = 0b00000000;	  //	all inputs
-	PCICR |= 0b00000001;  //	turn on interrupts for PORTB
-	PCMSK0 |= 0b00000001; //	turn on interrupts for specific pin (B8)
-	sei();				  //	enable interrupts again
+	cli();			//	disable interrupts
+	DDRB = 0b00000000;	//	all inputs
+	PCICR |= 0b00000001;	//	turn on interrupts for PORTB
+	PCMSK0 |= 0b00000001;	//	turn on interrupts for specific pin (B8)
+	sei();			//	enable interrupts again
 	Serial.begin(9600);
 }
 
@@ -14,11 +14,11 @@ void loop()
 {
 }
 
-ISR(PCINT0_vect) // ISR for PORTB
+ISR(PCINT0_vect)		// ISR for PORTB
 {
-	if (!(PINB & 1)) //	falling edge on B8
+	if (!(PINB & 1))	//	falling edge on B8
 	{
-		count++; //	count the interrupt
+		count++;	//	count the interrupt
 		Serial.println(count);
 	}
 }
