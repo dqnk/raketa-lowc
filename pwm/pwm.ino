69849f8ffa5182418424859d2f6c411555cef1a6
dqnk <64268180+dqnk@users.noreply.github.com>
2021-10-15 18:29:23 +0200

    new timer delays

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -8,6 +8,9 @@
  *
  */
 
+
+int time
+
 void setup()
 {
 
@@ -24,19 +27,20 @@ void setup()
 	TCNT1  = 0;	
 
 	// set compare match register for 1hz increments
-	// OCR1A = 16*10^6 / desired period - 1 (result must be <65536)
+	// OCR1A = 16*10^6 / (frequency * prescaler) - 1 (result must be <65536)
 
 	// 16e6/1024/1Hz - 1 
-	OCR1A = 15624;  
+	OCR1A = 31999;  
 
-	//	B
-	OCR1B = 1000;
+	//	B = 16000 + param
+	OCR1B = 16000 + time;
 
 	// turn on CTC mode - timer counter resets to 0
 	TCCR1B |= (1 << WGM12);
 
 	// Set 1024 prescaler with CS12 and CS10 bits - TCCR1B = 0b00000101
-	TCCR1B |= (1 << CS12) | (1 << CS10);  
+	// Set 1 prescaler with just (TCCR1B = 0b00000101
+	TCCR1B |= /*(1 << CS12) | */ (1 << CS10);  
 
 	// enable timer compare interrupt for timer TCNT1
 	TIMSK1 |= (1 << OCIE1B);
@@ -49,6 +53,7 @@ void setup()
 ISR(TIMER1_COMPB_vect)
 {
 	//	B interrupt
+	PORTB ^= 0b00000001;
 }
 
 void loop()
